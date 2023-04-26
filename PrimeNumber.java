2b1d6c6debf70ccb4ef5bc49afefe6f37e0be7de
gadhari209 <gadhari209@gmail.com>
2023-04-26 11:28:43 +0530

    ain

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,20 +6,20 @@ public class PrimeNumber {
 
 	public static void main(String[] args) 
 	{
-		int n=2;
+		int n=13;
 		int flag=0;
 		for( int i=2;i<=n/2;i++)
 		{
 			if(n%i==0)
 			{   flag=1;
-				System.out.println(" not prime");
+				System.out.println(" *****not*** prime");
 			    break;
 			}
 		  }
 				 
 				if(flag==0)
 		  
-			System.out.println("its prime number");
+			System.out.println("its**prime****number");
 
 	}}
 
